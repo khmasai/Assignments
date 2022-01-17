3a1caaa7ecd3e231985780c8ade2cb657b0cea1e
Khushboo <khush@Mohits-MacBook-Air.local>
2022-01-17 17:08:41 +0530

    pushing assignment 3 Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 
 import './App.css';
-import List from './Component/List';
-import AnotherList from './Component/AnotherList';
+import Join from './Components/JoinUs';
+import Setting from './Components/Settings';
+import Login from './Components/Login';
+import Contact from './Components/ContactUs';
+import Search from './Components/Search';
+import Help from './Components/Help';
+import Home from './Components/Home';
+import Download from './Components/Download';
 
 function App() {
   return (
-    <div className="App App-header">
-      <List/>
-      <AnotherList/>
+    <div className="App">
+      <div>
+        <Join/>
+        <Setting/>
+      </div>
+      <div> 
+        <Login/>
+        <Contact/>
+      </div>
+      <div>
+        <Search/>
+        <Help/>
+      </div>
+      <div>
+        <Home/>
+        <Download/>
+      </div>
     </div>
   );
 }
