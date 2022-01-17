9e44d97e6f3ecaf3800e867af324bd18d156c78a
Khushboo <khush@Mohits-MacBook-Air.local>
2022-01-17 17:25:37 +0530

    pushing to assignment-todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 
 import './App.css';
-import HeaderList from './Components/HeaderList';
-import TextList from './Components/TextList';
-import TempList from './Components/TempList';
-import PriceList from './Components/PriceList';
+import Todo from './Todo/Todo';
 
 function App() {
   return (
-    <>
     <div className="App">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNSqwCFx0cCZaSOaxHDrP2uXdzhj6MxQi9aQ&usqp=CAU" alt=""/>
-      <div className="i-tag">i</div>
-      <HeaderList/>
-      <TextList/>
-      <TempList/>
-      <PriceList/>
+       <Todo/>
     </div>
-    <div className="space"></div>
-    </>
   );
 }
 
