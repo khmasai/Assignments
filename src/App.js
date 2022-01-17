5553a4ca8787b135f959e611ed2c36105c355f66
Khushboo <khush@Mohits-MacBook-Air.local>
2022-01-17 19:11:47 +0530

    react useEffect API

--- src/App.js
+++ src/App.js
@@ -1,115 +1,102 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
 
-import './App.css';
-import data from "./data.json";
-import RestaurantDetails from './Components/RestaurantDetails/RestaurantDetails';
-import { useState } from 'react';
-import Pagination from './Components/Pagination';
+const UserCard = ({ id, name, url, avatar }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "2rem",
+        border: "2px solid red",
+        padding: "1rem",
+        margin: "1.5rem",
+        background: "antiquewhite"
+      }}
+    >
+      <img width="50" src={avatar} alt={id} />
+      <div>
+        <div>{name}</div>
+        <div>{url}</div>
+      </div>
+    </div>
+  );
+};
 
-function App() {
-  const [state, setState]=useState({
-      filterRating: 0,
-  })
-  const [payment, setPayment]=useState({
-     paymentMethod: "All",
-  })
-  const [cost, setCost]=useState({
-      sortMethod: null
-  })
-  const [page,setPage]= useState(1);
+const Pagination = ({ totalPages, currentPage, onClickCallback }) => {
+  const pages = new Array(totalPages).fill(0).map((a, i) =>
+    i + 1 === currentPage ? (
+      <button disabled style={{ background: "olive" }} key={i}>
+        {i + 1}
+      </button>
+    ) : (
+      <button onClick={() => onClickCallback(i + 1)} key={i}>
+        {i + 1}
+      </button>
+    )
+  );
+  return <div style={{ display: "flex", gap: "1rem",marginLeft: 30 }}>{pages}</div>;
+};
 
-  const handleRating=(rating)=>{
-    setState({filterRating: rating});
-  }
-  const handlePayment = (payment)=>{
-    setPayment({paymentMethod: payment})
-  }
-  
-  const handleSort=(order)=>{
-    setCost({sortMethod: order})
-  }
-
-  const changePageTo = (num) => {
-    if (num <= 1) {
-      setPage(1);
-      return;
-    }
-    setPage(num);
+// * useEffect( callback, dependency array)
+// * after render
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  console.log(`inside app`);
+  const getUsers = ({ query = "masai", page = 1 }) => {
+    return fetch(
+      `https://api.github.com/search/users?q=${query}&page=${page}`
+    ).then((res) => res.json());
   };
-  var perPage = 5;
-  // lowerBound
-  // upperBound
-  // const filteredResults = data.filter(
-  //   (_, i) => i >= (page - 1) * perPage && i < page * perPage
-  // );
+  // On Mounting
+  useEffect(() => {
+    getUsers({
+      page
+    })
+      .then((res) => {
+        setData(res);
+        if (res.total_count) {
+          const totalPages = Math.ceil(res.total_count / 30);
+          setTotalPages(totalPages);
+        }
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [page]);
+  // * EMPTY DEPENDANCY ARRAY
+  // if(isLoading){
+  //   return <h3>Loading...</h3>
+  // }
 
+  const handlePageChange = (value) => {
+    setPage(value);
+  };
   return (
     <div className="App">
-      <h1>RESTAURANT WEBSITE</h1>
-      <div style={{color: "gray",fontSize: "20px", fontWeight: "500"}}>
-        Ratings:
-        {
-          [4,3,2,1,0].map(rating=>{
-            return <button key={rating} onClick={()=> handleRating(rating)}>
-              {rating===0 ? "All" : rating}
-            </button>
-          })
-        }
-      </div>
-      <div style={{color: "gray",fontSize: "20px", fontWeight: "500"}}>
-        Payment Methods:
-        {
-          ["All", "Cash", "Card"].map(method=>{
-            return <button key={method} onClick={()=>handlePayment(method)}>{method}</button>
-          })
-        }
-      </div>
-      <div style={{color: "gray",fontSize: "20px", fontWeight: "500"}}>
-        Cost:
-        {
-           ["Low To High","High To Low","unsorted"].map(order=> {
-             return <button key={order} onClick={() =>handleSort(order)}>
-                {order}
-              </button>
-            })
-        }
-      </div>
-      <div>
-        <Pagination
-          currentPage={page}
-          onClickCallback={(page) => changePageTo(page)}
-          total={Math.floor(data.length/5)}
-        />
-      </div>
-      {
-        data
-        .filter(({rating,payment_methods}) => {
-          const { cash, card } = payment_methods;
-          let paymentCondition = true;
-          if (payment.paymentMethod === "Cash") {
-            paymentCondition = cash ? true : false;
-          } else if (payment.paymentMethod === "Card") {
-            paymentCondition = card ? true : false;
-          }
-          return rating >= state.filterRating && paymentCondition;
-        })
-        .filter(
-          (_, i) => i >= (page - 1) * perPage && i < page * perPage
-        )
-        .sort((a, b) =>{
-          if (cost.sortMethod === "Low To High") {
-            return a.costForTwo - b.costForTwo;
-          }
-          if (cost.sortMethod === "High To Low") {
-            return b.costForTwo - a.costForTwo;
-          }
-          return 0;
-        })
-        .map((item) =>{
-          return <RestaurantDetails data={item} key={item.id}/>
-        })
-      }
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          {data?.items?.map((user) => (
+            <UserCard
+              key={user.id}
+              id={user.login}
+              name={user.login}
+              url={user.url}
+              avatar={user.avatar_url}
+            />
+          ))}
+          <Pagination
+            currentPage={page}
+            onClickCallback={handlePageChange}
+            totalPages={totalPages}
+          />
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
