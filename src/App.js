3b0405b81dad75776cbe7a0b50821154884eaaaf
Khushboo <khush@Mohits-MacBook-Air.local>
2022-01-17 19:27:47 +0530

    react-events-i

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 
 import './App.css';
-import {useRef} from "react";
+import Form from './Components/Form';
 
 function App() {
-  const ref=useRef(null);
-
-  const handleClick=()=>{
-     ref.current.focus()
-     console.log(ref)
-  }
-
   return (
     <div className="App">
-        <h1>Focus - input - useRef</h1>
-        <div>
-          <input ref={ref} placeholder="Type Something" type="text"/>
-          <button onClick={handleClick}>Click</button>
-        </div>
+      <h1>Form, Events - ( I )</h1>
+      <Form/>
     </div>
   );
 }
