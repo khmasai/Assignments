793a8faa99b0135a331d1aeb00a350cee4136aab
Khushboo <khush@Mohits-MacBook-Air.local>
2022-01-17 19:22:05 +0530

    react useRef

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import "./styles.css";
-import { useEffect, useState } from "react";
 
-const Countdown = ({ initial }) => {
-  const [value, setValue] = useState(initial);
-  // * disptach(newValue)
-  // * dispatch(prev=>prev+1)
-  useEffect(() => {
-    const id = setInterval(() => {
-      console.log(Date.now());
-      setValue((prev) => {
-        if (prev - 1 === 0) {
-          clearInterval(id);
-        }
-        return prev - 1;
-      });
-    }, 1000);
-    return () => {
-      console.log(`cleaning up...`);
-      clearInterval(id);
-    }; // cleanup function
-  }, []);
-  return (
-    <div>
-      <h3>Countdown</h3>
-      <h3>{value}</h3>
-    </div>
-  );
-};
+import './App.css';
+import {useRef} from "react";
 
-// * useEffect( callback, dependency array)
-// * after render
-export default function App() {
-  const [hide, setHide] = useState(false);
+function App() {
+  const ref=useRef(null);
+
+  const handleClick=()=>{
+     ref.current.focus()
+     console.log(ref)
+  }
+
   return (
     <div className="App">
-      {!hide && <Countdown initial={5}/>}
-      <button onClick={() => setHide(!hide)}>TOGGLE HIDE</button>
+        <h1>Focus - input - useRef</h1>
+        <div>
+          <input ref={ref} placeholder="Type Something" type="text"/>
+          <button onClick={handleClick}>Click</button>
+        </div>
     </div>
   );
 }
+
+export default App;
